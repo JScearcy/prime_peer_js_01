b01b1fd87d676e13e747a2759e6ecb000f5dd137
Jason Spidle <panoptican@hairball.local>
2015-08-11 16:11:48 -0500

    Peer programming challenge completed.

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -1,35 +1,95 @@
 /*
-1. Start with the number 42 and set that equal to 'value'
+1. Start with the number 42 and set that equal to 'value'*/
 
-2. Create condition logic to check if the value is great or equal to 53
+var value = 42;
+
+/*2. Create condition logic to check if the value is great or equal to 53
 2-1. If true, add 42 to 'value'
-2-2. If false, subtract 13 from 'value'
+2-2. If false, subtract 13 from 'value'*/
 
-3. Create a string that is set to 11, add it to 'value'
+if(value >= 53) {
+	value += 42;
+} else {
+	value -= 13;
+}
 
-4. Create an array, loop through 'value' using charAt, set array[i] to each value
+/*3. Create a string that is set to 11, add it to 'value'*/
 
-5. Remove the first and last values off the array
+var string = '11';
+value += string;
 
-6. Create a new variable. Loop backwards through the array and store each value into the new Variable, combining each of the values of that array (backwards remember!)
+/*4. Create an array, loop through 'value' using charAt, set array[i] to each value*/
 
-7. parseInt both the 'value' and the new variable created in Step 6, ensure that both 'value' and the new variable are set to these new parsed values;
+var array = [];
+for(var i = 0; i < value.length; i++) {
+	array[i] = value.charAt(i);
+}
 
-******* SWITCH PROGRAMMERS *******
+/*5. Remove the first and last values off the array*/
 
-8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
+array.pop();
+array.shift();
 
-9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27. 
-If neither of these are true, set the value to 2.
+/*6. Create a new variable. Loop backwards through the array and store each value into the new Variable, combining each of the values of that array (backwards remember!)*/
 
-10. Create a while loop that counts down from 10 and increments 'value' by 1.
+var newVal = "";
 
-11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
-is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
+for(i = array.length-1; i >= 0; i--) {
+	newVal += array[i];
+}
 
-12. Call the function.
+/*7. parseInt both the 'value' and the new variable created in Step 6, ensure that both 'value' and the new variable are set to these new parsed values;*/
 
-13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
+value = parseInt(value);
+newVal = parseInt(newVal);
 
-14. Your answer should be a String value that equals 7. Is that what you got?
+/******** SWITCH PROGRAMMERS *******
+
+/*8. Add 'value' and the new variable created in Step 6 together and store them in 'value'*/
+
+value += newVal;
+
+/*9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27. 
+If neither of these are true, set the value to 2.*/
+
+if (value < 60){
+	value = 14;
+}
+else if (value == 2930){
+	value = 27;
+}
+else {
+	value = 2;
+}
+
+/*10. Create a while loop that counts down from 10 and increments 'value' by 1.*/
+
+i = 10
+while (i > 0){
+	value += 1;
+	i--;
+}
+
+/*11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.*/
+
+function stringDrop(val){
+	var val1 = "";
+	val = val.toString();
+	if (val.length > 1){
+		for(i = 1; i < val.length; i++) {
+			val1 += val[i];
+		}
+	}
+	return val1;
+}
+
+/*12. Call the function.*/
+
+value = stringDrop(value);
+
+/*13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.*/
+
+console.log(value);
+
+/*14. Your answer should be a String value that equals 7. Is that what you got?
 */
